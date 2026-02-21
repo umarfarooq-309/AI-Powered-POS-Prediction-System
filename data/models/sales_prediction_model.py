8c9e9f866e05d65e81428409d7ab99b04217218d
umarfarooq-309 <mumarfarooqakram@gmail.com>
2026-02-21 02:20:06 -0800

    Update sales_prediction_model.py

--- data/models/sales_prediction_model.py
+++ data/models/sales_prediction_model.py
@@ -1,1 +1,16 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
+data = pd.read_csv("data/sample_sales_data.csv")
+
+X = data[['Quantity', 'Price']]
+y = data['Sales']
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
+
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+score = model.score(X_test, y_test)
+print("Model Accuracy:", score)
